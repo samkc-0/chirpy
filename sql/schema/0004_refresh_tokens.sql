c59eb334e75984ccef7de7f14ce74f3a73263a47
samkc-0 <sam.k.c.0@icloud.com>
2025-10-09 16:55:32 +0100

    create refresh tokens table

---
+++ sql/schema/0004_refresh_tokens.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+create table refresh_tokens (
+  token text primary key,
+  user_id uuid not null,
+  expires_at timestamp default now() + interval '60 days',
+  revoked_at timestamp,
+  created_at timestamp default now(),
+  updated_at timestamp default now()
+);
+
+-- +goose Down
+drop table refresh_tokens;

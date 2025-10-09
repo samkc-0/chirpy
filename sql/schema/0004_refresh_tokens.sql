150465a193be3229b4f5e579c4713251d6c8e917
samkc-0 <sam.k.c.0@icloud.com>
2025-10-09 16:57:49 +0100

    make user_id a foregin key in refresh tokens table

--- sql/schema/0004_refresh_tokens.sql
+++ sql/schema/0004_refresh_tokens.sql
@@ -5,7 +5,8 @@ create table refresh_tokens (
   expires_at timestamp default now() + interval '60 days',
   revoked_at timestamp,
   created_at timestamp default now(),
-  updated_at timestamp default now()
+  updated_at timestamp default now(),
+  foreign key (user_id) references users(id) on delete cascade
 );
 
 -- +goose Down
